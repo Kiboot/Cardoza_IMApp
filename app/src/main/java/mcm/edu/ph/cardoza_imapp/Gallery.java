165f3a1a39b5040cd28cf81ec9ebebceda330ecd
Prg <kevinlee.cardoza@gmail.com>
2021-01-12 10:58:09 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/cardoza_imapp/Gallery.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.cardoza_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery);
+    }
+}
